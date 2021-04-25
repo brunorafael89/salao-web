02839bb728bec867149bcb4e356f74ca70543f48
Bruno Mendes <brpimendes@gmail.com>
2021-04-24 23:48:22 -0300

    Ajuste no tamanho do menu lateral fixando em 320 pixels, evitando que a barra ajuste o tamanho ao reduzir a tela. Ajuste no cabecalho fixando no topo evitando que a barra role quando as informacoes sao grandes para caber na tela.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -12,31 +12,29 @@ import "./styles.css";
 function Header(){
     return (
         <header>
-            <div className="menu-fixed">
-                <div className="contacts">
-                    <div className="logo">
-                        <img src={logo} alt="logo" />
-                    </div>
-
-                    <div className="direct-contacts">
-                       <span className="material-icons"><FiPhone/></span> <p> (21)2404-0695 </p>
-                       <span className="material-icons"><FiMail/></span> <p> E-mail: beauty_salao@gmail.com </p>
-                    </div>
-
-                    <div className="hamburguer">
-                        <span className="material-icons">menu</span>
-                    </div>
-
-                    <nav>
-                        <ul>
-                            <li><span className="material-icons"><MdExitToApp/></span><a href="index.html">Sair</a></li>
-                            <li><span className="material-icons"><AiOutlineHome/></span><a href="principal.html">Página Principal</a></li>
-                            <li><span className="material-icons"><RiMoneyDollarBoxLine/></span><a href="tabelaPreco.html">Tabela de Preços</a></li>
-                            <li><span className="material-icons"><AiOutlineShoppingCart/></span><a href="#contact">Carrinho</a></li>
-                        </ul>
-                    </nav>
+            <div className="contacts">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
                 </div>
-            </div>
+
+                <div className="direct-contacts">
+                    <span className="material-icons"><FiPhone/></span> <p> (21)2404-0695 </p>
+                    <span className="material-icons"><FiMail/></span> <p> E-mail: beauty_salao@gmail.com </p>
+                </div>
+
+                <div className="hamburguer">
+                    <span className="material-icons">menu</span>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li><span className="material-icons"><MdExitToApp/></span><a href="index.html">Sair</a></li>
+                        <li><span className="material-icons"><AiOutlineHome/></span><a href="principal.html">Página Principal</a></li>
+                        <li><span className="material-icons"><RiMoneyDollarBoxLine/></span><a href="tabelaPreco.html">Tabela de Preços</a></li>
+                        <li><span className="material-icons"><AiOutlineShoppingCart/></span><a href="#contact">Carrinho</a></li>
+                    </ul>
+                </nav>
+                </div>
         </header>
     )
 }
