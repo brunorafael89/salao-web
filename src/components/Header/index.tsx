fc590d27b4ff5cf4688f156e9abc72b0e73d016e
Bruno Mendes <brpimendes@gmail.com>
2021-06-09 20:06:33 -0300

    Ajustes no cabecalho para aparelhos moveis

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,10 +7,8 @@ import { RiMoneyDollarBoxLine } from "react-icons/ri";
 import { MdExitToApp } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 import logo from "../../assets/images/logo.png";
-import coloracao from "../../assets/images/coloração1.jpg";
 
 import "./styles.css";
-import { FaTrashAlt } from "react-icons/fa";
 
 function Header(){
     return (
@@ -35,57 +33,11 @@ function Header(){
                             <li><span className="material-icons"><MdExitToApp/></span><a href="/">Sair</a></li>
                             <li><span className="material-icons"><AiOutlineHome/></span><a href="home">Página Principal</a></li>
                             <li><span className="material-icons"><RiMoneyDollarBoxLine/></span><a href="TabelaPreco">Tabela de Preços</a></li>
-                            <li><span className="material-icons"><AiOutlineShoppingCart/></span><a href="#contact">Carrinho</a></li>
+                            <li><span className="material-icons"><AiOutlineShoppingCart/></span><a href="Carrinho">Carrinho</a></li>
                         </ul>
                     </nav>
-                    </div>
+                </div>
             </header>
-
-            <div className="cart">                
-                <div className="cart-items">
-                    <div className="cart-img">
-                        <img src={coloracao} alt="coloração" />
-                    </div>
-
-                    <div className="cart-service">
-                        <span className="item-service">Cabelo Longo</span>
-                        <span className="item-data">19/05/2021</span>
-                        <span className="item-hora">15:30</span>
-                    </div>
-
-                    <div className="cart-value">
-                        <span className="item-valor">R$ 250,00</span>
-                    </div>
-
-                    <div className="cart-excluir">
-                        <button type="submit"><FaTrashAlt/></button>
-                    </div>
-                </div>
-
-                <div className="cart-items">
-                    <div className="cart-img">
-                        <img src={coloracao} alt="coloração" />
-                    </div>
-
-                    <div className="cart-service">
-                        <span className="item-service">Cabelo Longo</span>
-                        <span className="item-data">19/05/2021</span>
-                        <span className="item-hora">15:30</span>
-                    </div>
-
-                    <div className="cart-value">
-                        <span className="item-valor">R$ 250,00</span>
-                    </div>
-
-                    <div className="cart-excluir">
-                        <button type="submit"><FaTrashAlt/></button>
-                    </div>
-                </div>
-
-                <div className="cart-finalizar">
-                    <button>Finalizar compra</button>
-                </div>
-            </div>
         </>
     )
 }
