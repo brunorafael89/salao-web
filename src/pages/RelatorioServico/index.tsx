84be7cb358ebfb156500542117b063f26d47abd1
Bruno Mendes <brpimendes@gmail.com>
2021-06-18 16:29:17 -0300

    Retirado o campo forma de pagamento na tela de agendamento e implementacao da funcao de Relatorio de servico na tela de relatorios

--- src/pages/RelatorioServico/index.tsx
+++ src/pages/RelatorioServico/index.tsx
@@ -4,7 +4,6 @@ import Header from "../../components/Header";
 import MenuLateral from "../../components/MenuLateral";
 import { toast } from "react-toastify";
 import api from "../../services/api";
-import {IoMdPrint} from "react-icons/io";
 import {RiFileExcel2Line} from "react-icons/ri";
 
 import { CSVLink } from "react-csv";
@@ -13,26 +12,28 @@ import "./styles.css";
 
 function RelatorioServicoPage(){
     const [relatorioServicos, setRelatorioServico] = useState([]);
-    const [agendamentos, setAgendamentos] = useState([]);
+    // const [agendamentos, setAgendamentos] = useState([]);
     const [servicos, setServicos] = useState([]);
     const [IdServicos, setIdServicos] = useState("");
     const [profissionais, setProfissionais] = useState([]);
     const [idProfissional, setIdProfissional] = useState('');
+    const [dataFrom, setDataFrom] = useState(format(new Date(), "yyyy-MM-dd"));
+    const [dataTo, setDataTo] = useState(format(new Date(), "yyyy-MM-dd"));
 
     useEffect(() => {
-        getAgendamentos() 
+        // getAgendamentos() 
         getProfissional()
         getServicos()
     }, [])
 
-    async function getAgendamentos(){
-        try {
-            const response = await api.get("agendamento"); 
-            setAgendamentos(response.data)
-        } catch(err) {
-            toast.error("Erro ao consultar a agenda");
-        }
-    } 
+    // async function getAgendamentos(){
+    //     try {
+    //         const response = await api.get("agendamento"); 
+    //         setAgendamentos(response.data)
+    //     } catch(err) {
+    //         toast.error("Erro ao consultar a agenda");
+    //     }
+    // } 
 
     async function getServicos(){
         try {
@@ -50,12 +51,12 @@ function RelatorioServicoPage(){
 
     async function geraRelatorio(e: FormEvent){
         e.preventDefault();
-        const response = await api.get("relatorio/servico");
+
+        const response = await api.get(`relatorio/servico/${idProfissional}/${IdServicos}/${dataFrom}/${dataTo}`);
+
         setRelatorioServico(response.data)
     }
 
-        // Deixei estes dados como exemplo para saber se o botão de exportar está funcionando
-    // é necessário trazer as informações do banco de dados
     const csvData = [
         ["Nome", "Função", "CPF"],
         ["Carlos Augusto", "Cabeleireiro", "123123123"],
@@ -73,46 +74,53 @@ function RelatorioServicoPage(){
                     <h1>Relatorio de Serviços</h1>
                 
                     <section className="info-select">
-                        <div className="select-servico">
-                            <label htmlFor="servicos">
-                                <span className="spn-titulo">Selecione o serviço</span>
-                                <select name="servico" id="" onChange={(e) => setIdServicos(e.target.value)}>
-                                    <option value="">Selecione o serviço</option>
-                                    {servicos.map((servico: any) => (
-                                            <option value={JSON.stringify(servico)}>{servico.nome}</option>
+                        <form onSubmit={geraRelatorio}>
+                            <div className="select-servico">
+                                <label htmlFor="servicos">
+                                    <span className="spn-titulo">Selecione o serviço</span>
+                                    <select name="servico" onChange={(e) => setIdServicos(e.target.value)}>
+                                        <option value="">Selecione o serviço</option>
+                                        {servicos.map((servico: any) => (
+                                            <option value={servico.servico_id}>{servico.nome}</option>
                                         ))}
-                                </select>
-                            </label>
-                        </div>
-        
-                        <div className="select-profissional">
-                            <label htmlFor="">
-                                <span className="spn-titulo">Selecione o profissional</span>
-                                <select name="profissional" id="" onChange={(e) => setIdProfissional(e.target.value)}>
+                                    </select>
+                                </label>
+                            </div>
+            
+                            <div className="select-profissional">
+                                <label htmlFor="">
+                                    <span className="spn-titulo">Selecione o profissional</span>
+                                    <select name="profissional" onChange={(e) => setIdProfissional(e.target.value)}>
                                         <option value="">Selecione o profissional</option>
                                         {profissionais.map((profissional: any) => (
                                             <option value={profissional.profissional_id}>{profissional.nome}</option>
                                         ))}
-                                </select>
-                            </label>
-                        </div>
-        
-                        <div className="select-data">
-                            <span className="spn-titulo">Selecione o período</span>
-                            <form onSubmit={geraRelatorio}>
+                                    </select>
+                                </label>
+                            </div>
+            
+                            <div className="select-data">
+                                <span className="spn-titulo">Selecione o período</span>
+
                                 <label htmlFor="">
                                     <span>De:</span>
-                                    <input type="date" />
+                                    <input 
+                                        type="date" 
+                                        onChange={(e)=>setDataFrom(e.target.value)}
+                                    />
                                 </label>
 
                                 <label htmlFor="">
                                     <span>Até:</span>
-                                    <input type="date" />
+                                    <input 
+                                        type="date" 
+                                        onChange={(e)=>setDataTo(e.target.value)}
+                                    />
                                 </label>
 
                                 <button className="buttons" type="submit">Gerar Relatório</button>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </section>
 
                     <div className="table">
@@ -140,22 +148,14 @@ function RelatorioServicoPage(){
 
                     <div className="buttons-export">
                         <label htmlFor="">
-                            <button className="buttons" type="submit">Imprimir</button>
-                            <span className="material-icons"><IoMdPrint/></span>
-                        </label>
-
-                        <label htmlFor="">
-                            {/* {relatorioComissoes.map((relatorioComissao: any) => ( */}
-                                <CSVLink
-                                    className="buttons"
-                                    data={csvData}
-                                    filename={`relatorioServico.csv`}
-                                    separator=";"
-                                    // filename={`relatorioComissao${relatorioComissao.nome}.csv`}
-                                >
-                                    Exportar para Excel
-                                </CSVLink>
-                            {/* ))} */}
+                            <CSVLink
+                                className="buttons"
+                                data={csvData}
+                                filename={`relatorioServico.csv`}
+                                separator=";"
+                            >
+                                Exportar para Excel
+                            </CSVLink>
                             <span className="material-icons"><RiFileExcel2Line/></span>
                         </label>
                     </div>
